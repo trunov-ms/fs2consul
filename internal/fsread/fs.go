7a365ebbb1138c1b6036350fb43bb769b473538d
Трунов Максим <trunov_ms@taximaxim.ru>
2021-10-28 09:39:43 +0500

    fix excludes bug

--- internal/fsread/fs.go
+++ internal/fsread/fs.go
@@ -49,22 +49,22 @@ func getKV(basedir string) []KVPair {
 	var output []KVPair
 	err := filepath.Walk(basedir,
 		func(path string, info os.FileInfo, err error) error {
-			if !info.IsDir() {
-				if !strings.Contains(path, ".git") { // exclude files from .git dir(if exist)
-					if isTxtFile(path) { // filtering by content type
-						content, err := os.ReadFile(path)
-						if err != nil {
-							return err
-						}
-						if strings.HasSuffix(string(content), "\n") {
-							content = []byte(strings.TrimSuffix(string(content), "\n"))
-						}
-						kvp := KVPair{
-							Key:   strings.TrimPrefix(path, basedir),
-							Value: string(content),
-						}
-						output = append(output, kvp)
+			// exclude directories and files from .git folder (if exist)
+			if !info.IsDir() && !strings.Contains(path, "/.git/") {
+				// filtering by content type
+				if isTxtFile(path) {
+					content, err := os.ReadFile(path)
+					if err != nil {
+						return err
 					}
+					if strings.HasSuffix(string(content), "\n") {
+						content = []byte(strings.TrimSuffix(string(content), "\n"))
+					}
+					kvp := KVPair{
+						Key:   strings.TrimPrefix(path, basedir),
+						Value: string(content),
+					}
+					output = append(output, kvp)
 				}
 			}
 			return nil
